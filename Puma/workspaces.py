6fc7043a4d0a282c62d362465bc0910f9c0f7096
dario <dario.rodriguez9378@hotmail.com>
2021-09-29 21:38:37 -0500

    Added general problem setting with Pyomo, Installed glpk utils open source solver.

--- Puma/workspaces.py
+++ Puma/workspaces.py
@@ -12,3 +12,8 @@ import sys
 ws1 = puma.Workspace.from_shape((10, 11, 12))
 print(f'Shape of workspace: {ws1.matrix.shape}')
 print(f'Unique values in matrix: {ws1.unique_values()}')
+
+# Define a workspace full of zeros of shape 20x31x212
+ws2 = puma.Workspace.from_shape_value((20, 31, 212), 1)
+print(f'Shape of workspace: {ws2.matrix.shape}')
+print(f'Unique values in matrix: {ws2.unique_values()}')
